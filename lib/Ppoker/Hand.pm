4cbc014fbe63cf2a2703c8f02d4eb6c687ffae43
Victor Frandsen <mrweektor@gmail.com>
2012-08-21 23:46:40 -0700

    started on hand evaluator

--- lib/Ppoker/Hand.pm
+++ lib/Ppoker/Hand.pm
@@ -3,17 +3,53 @@ package Ppoker::Hand;
 use strict;
 use Ppoker::Card;
 
-my @hands = (
-	"high card",
-	"pair",
-	"two pair",
-	"three of a kind",
-	"straight",
-	"flush",
-	"full house",
+use constant {
+	STRAIGHT_FLUSH => 1,
+	FOUR_OF_A_KIND => 2,
+	FULL_HOUSE => 3,
+	FLUSH => 4,
+	STRAIGHT => 5,
+	THREE_OF_A_KIND => 6,
+	TWO_PAIR => 7,
+	ONE_PAIR => 8,
+	HIGH_CARD => 9,
+	
+	CLUB => 0x8000,
+	DIAMOND => 0x4000,
+	HEART => 0x2000,
+	SPADE => 0x1000,
+	
+	TWO => 0,
+	THREE => 1,
+	FOUR => 2,
+	FIVE => 3,
+	SIX => 4,
+	SEVEN => 5,
+	EIGHT => 6,
+	NINE => 7,
+	TEN => 8,
+	JACK => 9,
+	QUEEN => 10,
+	KING => 11,
+	ACE => 12,
+};
+
+my @hand_values = (
+	"straight flush",
 	"four of a kind",
-	"straight flush"
+	"full house",
+	"flush",
+	"straight",
+	"three of a kind",
+	"two pair",
+	"pair",
+	"high card",
 );
+
+sub rank {
+	my $x = shift;
+	return (($x >> 8) & 0xF)
+}
 
 # given a players hand and any community cards what is the players best hand?
 sub whichHand {
